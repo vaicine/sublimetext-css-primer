13c49b7750241f7d7df50181e0b7e25da82feb8b
James Harries <vaicine@vaicine.com>
2013-10-20 23:41:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# CSS Primer for Sublime Text 2/3
+
+CSS Primer - Converts your HTML file into CSS IDs/Classes and puts them in a stylesheet file ready for your styling
+
+## Installation
+
+### [Sublime Package Control](http://wbond.net/sublime_packages/package_control)
+
+In the command Pallette choose **Package Control: Install Repository** and select **CSS Primer**
+
+### Git installation
+
+Clone the repository in your Sublime Text "Packages" directory:
+
+    git clone https://github.com/vaicine/sublime-css-primer.git "CSS Primer"
+
+The "Packages" directory is located at:
+
+* OS X:
+
+        ~/Library/Application Support/Sublime Text %your-sublime-version%/Packages/
+
+* Linux:
+
+        ~/.config/sublime-text-%your-sublime-version%/Packages/
+
+* Windows:
+
+        %APPDATA%/Sublime Text %your-sublime-version%/Packages/
+
+### In Command Palette:
+
+* **CSSPrimer: Prime CSS from file** - creates a new file in the same folder using the same name as the source ending with '.css' *
+
---
+++ cssprimer.py
@@ -0,0 +1,39 @@
+import re
+import sublime, sublime_plugin
+
+class CssPrimerFromFileCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    source = self.view.file_name()
+    target = source.sub('.html', '') + '.css'
+    if target:
+      with open(source, 'r') as f:
+        html = f.read()
+
+      css = CSSPrimer.html2css(html)
+
+      with open(target, 'w') as f:
+        f.write(css)
+      self.view.window().open_file(target)
+
+  def is_enabled(self):
+    return True 
+
+
+class CSSPrimer:
+  @classmethod
+  def html2css(self, html):
+    id_attributes = set(re.findall(r'id=["|\']([^"|\']*)["|\']', html))
+    class_attributes = set(re.findall(r'class=["|\']([^"|\']*)["|\']', html))
+
+    stylesheet = ''
+    if id_attributes:
+      for id_attribute in id_attributes:
+        id_string = "#" + id_attribute + " {\n\n}\n\n"
+        stylesheet = stylesheet + id_string
+
+    if class_attributes:
+      for class_attribute in [classes for segments in class_attributes for classes in segments.split()]:
+        class_string = "." + class_attribute + " {\n\n}\n\n"
+        stylesheet = stylesheet + class_string
+
+    return stylesheet
